0ceb9933250b861bb6277d4ef08d215bc6cf2ff1
varnitasr <varnitasr20@gmail.com>
2023-11-07 12:02:19 +0530

    first commit

---
+++ calci.py
@@ -0,0 +1,10 @@
+import math
+class calculator:
+   def add(self, a, b):
+    return a+b
+   def substract(self, a, b):
+    return a-b
+    def squareRoot(self, a):
+        return math.sqrt(a)
+    def multiply(a, b):
+      return a*b
